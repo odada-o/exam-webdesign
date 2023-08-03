1fb149ddd60b7adec14782c3e25de5763029483f
odada <eehd80yul@gmail.com>
2023-08-03 13:22:06 +0900

    slide

--- 실기-완성/0-A-1-just쇼핑몰-slider-script/js/comm.js
+++ 실기-완성/0-A-1-just쇼핑몰-slider-script/js/comm.js
@@ -22,17 +22,38 @@ $(function () {
   });
 
   // tabs
+  const tabEl = $(".tab-item");
+  const tabTit = tabEl.find(".tit");
+
   // .tab-item 옆에 .on 삭제
-  $(".tab-item").removeClass("on");
+  tabEl.removeClass("on");
   // .tab-item:first-child 에게 .on 추가
-  $(".tab-item:first-child").addClass("on");
+  // $(".tab-item:first-child").addClass("on");
+  tabEl.first().addClass("on");
 
   // .tab-item .tit 을 클릭했을 때
-  $(".tab-item .tit").on("click", function () {
+  tabTit.on("click", function () {
     // .tab-item에 붙어 있는 .on 삭제
-    $(".tab-item").removeClass("on");
+    tabEl.removeClass("on");
 
     // 방금 클릭한 나(this)의 부모(parent())에게 .on 추가
     $(this).parent().addClass("on");
   });
+
+  // slide
+  let now = 1;
+
+  // 3초 마다 실행
+  setInterval(function () {
+    slide();
+  }, 3000);
+
+  function slide() {
+    console.log("dd");
+    // .lst-slide를 애니메이션(top: -300px)
+    // 3초 후에 -100% 6초 후에 -200%
+    $(".lst-slide").animate({
+      top: 100 * now * -1 + "%",
+    });
+  }
 });
