b4c1f277e62d0d48080c9da74aca0e093512ce8a
odada <eehd80yul@gmail.com>
2023-08-07 11:24:16 +0900

    nav jquery

--- 실기-완성/D-1-조이컨트리클럽/js/comm.js
+++ 실기-완성/D-1-조이컨트리클럽/js/comm.js
@@ -1,3 +1,17 @@
 $(function () {
-  // tabs
+  // nav
+  // #gnb .link-depth1 을 클릭했을 때
+  // $("#gnb .link-depth1").on("click", function(){
+  //   // .depth1 > li 에 있는 .active 삭제
+  //   $(".depth1 > li").removeClass("active");
+  //   // 내가 클릭한 아이의 엄마에게 .active 추가
+  //   $(this).parent().addClass("active")
+  //   // $(this).parents("li").addClass("active")
+  // })
+
+  // 조건문
+  $("#gnb .link-depth1").on("click", function(){
+    
+
+  })
 });
