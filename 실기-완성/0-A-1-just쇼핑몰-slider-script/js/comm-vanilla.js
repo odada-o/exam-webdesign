3fba92fed1d092d6d899b59e2694e3493a885eb0
oddoddo <eehd80yul@gmail.com>
2023-12-28 07:28:26 +0900

    js 수정

---
+++ 실기-완성/0-A-1-just쇼핑몰-slider-script/js/comm-vanilla.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // 모달
+    const modalWrap = document.querySelector('#modal');
+    const modalBtn = modalWrap.querySelector('button');
+    const listbbsEl = document.querySelectorAll('.lst-bbs li');
+
+    modalWrap.classList.remove('active');
+
+    listbbsEl[0].addEventListener('click', function () {
+        modalWrap.classList.add('active');
+    });
+
+    modalBtn.addEventListener('click', function () {
+        modalWrap.classList.remove('active');
+    });
+
+    // 탭
+    const tabEl = document.querySelectorAll('.tab-item');
+
+    tabEl.forEach((el) => el.classList.remove('on'));
+    tabEl[0].classList.add('on');
+
+    tabEl.forEach((el) => {
+        el.querySelector('.tit').addEventListener('click', function () {
+            tabEl.forEach((innerEl) => innerEl.classList.remove('on'));
+            this.parentNode.classList.add('on');
+        });
+    });
+
+    // 슬라이드
+    let now = 0;
+    const slideCount = document.querySelectorAll('.lst-slide li').length;
+    const lstSlide = document.querySelector('.lst-slide');
+
+    setInterval(function () {
+        slide();
+    }, 3000);
+
+    function slide() {
+        now = (now + 1) % slideCount;
+        lstSlide.style.top = 100 * now * -1 + '%';
+    }
+});
--- 실기-완성/0-A-1-just쇼핑몰-slider-script/js/comm.js
+++ 실기-완성/0-A-1-just쇼핑몰-slider-script/js/comm.js
@@ -1,81 +1,41 @@
 $(function () {
-  // modal
+    // 모달
+    const modalWrap = $('#modal');
+    const modalBtn = modalWrap.find('button');
+    const listbbsEl = $('.lst-bbs li');
 
-  const modalWrap = $("#modal");
-  //$("#modal").find("button")
-  const modalBtn = modalWrap.find("button");
-  const listbbsEl = $(".lst-bbs li:first-child");
+    modalWrap.removeClass('active');
 
-  modalWrap.removeClass("active");
-  // modal 열기
-  // .lst-bbs li:first-child 를 클릭했을 때
-  listbbsEl.on("click", function () {
-    // #modal에 클래스 active를 추가
-    modalWrap.addClass("active");
-  });
+    listbbsEl.on('click', function () {
+        modalWrap.addClass('active');
+    });
 
-  // modal 닫기
-  // #modal button 을 클릭했을 때
-  modalBtn.on("click", function () {
-    // #modal에 .active를 삭제
-    modalWrap.removeClass("active");
-  });
+    modalBtn.on('click', function () {
+        modalWrap.removeClass('active');
+    });
 
-  // tabs
-  const tabEl = $(".tab-item");
-  const tabTit = tabEl.find(".tit");
+    // 탭
+    const tabEl = $('.tab-item');
 
-  // .tab-item 옆에 .on 삭제
-  tabEl.removeClass("on");
-  // .tab-item:first-child 에게 .on 추가
-  // $(".tab-item:first-child").addClass("on");
-  tabEl.first().addClass("on");
+    tabEl.removeClass('on');
+    tabEl.first().addClass('on');
 
-  // .tab-item .tit 을 클릭했을 때
-  tabTit.on("click", function () {
-    // .tab-item에 붙어 있는 .on 삭제
-    tabEl.removeClass("on");
+    $('.tab-item .tit').on('click', function () {
+        tabEl.removeClass('on');
+        $(this).parent().addClass('on');
+    });
 
-    // 방금 클릭한 나(this)의 부모(parent())에게 .on 추가
-    $(this).parent().addClass("on");
-  });
+    // 슬라이드
+    let now = 0;
+    const slideCount = $('.lst-slide li').length;
+    const lstSlide = $('.lst-slide');
 
-  // slide
-  let now = 1;
-  const slideEls = $(".lst-slide");
-  let height = 100;
-
-  // 3초 마다 실행
-  setInterval(function () {
-    slide();
-  }, 3000);
-
-  // 슬라이드 기능 만들기
-  function slide() {
-    console.log("dd");
-    // 조건문
-    // now : 0 - 1번째 슬라이드
-    // now : 1 - 2번째 슬라이드
-    // now : 2 - 3번째 슬라이드
-    // 참: 만약 1, 2번째 슬라이드일 경우
-    // 거짓 : 3번 슬라이드일 경우
-    if (now < 3) {
-      // 참 일 경우
-      // 다음 슬라이드로 이동
-      // .lst-slide를 애니메이션(top: -300px)
-      // 3초 후에 -100% 6초 후에 -200%
-      slideEls.animate({
-        top: height * now * -1 + "%",
-      });
-      // 변수 재선언
-      now = now + 1;
-    } else {
-      // 거짓 일 경우
-      // 첫번째 슬라이드로 이동
-      slideEls.animate({
-        top: 0,
-      });
-      now = 1;
+    function updateSlidePosition() {
+        lstSlide.css('top', 100 * now * -1 + '%');
     }
-  }
+
+    setInterval(function () {
+        now = (now + 1) % slideCount;
+        updateSlidePosition();
+    }, 3000);
 });
